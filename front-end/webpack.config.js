1b7e7fdabeeaec4900144e8fd2db20a17fbf811c
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-01-30 13:12:31 -0300

    Configuando manualmente o projeto - Babel e Webpack

---
+++ front-end/webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [".js",".jsx"]
+    },
+    module:{
+        rules: [
+            {
+                test: /.js?$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, './src'),
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015', 'react']
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    devServer: {
+        publicPath: "/",
+        contentBase: "./dist",
+    }
+};
